ce9024a6417c2d905211124b52f04bda0400687b
cjodra14 <64621611+cjodra14@users.noreply.github.com>
2021-10-20 02:48:43 +0200

    EjemploToolBar finalizado, creacionLVGVS(Fuentify)

--- EjemploToolBar/app/src/main/java/com/jodra/ejemplotoolbar/MainActivity.kt
+++ EjemploToolBar/app/src/main/java/com/jodra/ejemplotoolbar/MainActivity.kt
@@ -9,7 +9,9 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.SearchView
 import android.widget.Toast
+import androidx.appcompat.widget.ShareActionProvider
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuItemCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,11 +39,15 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu, menu)
         val itemBusqueda = menu?.findItem(R.id.btnBusqueda)
         val vistaBusqueda = itemBusqueda?.actionView as androidx.appcompat.widget.SearchView
+        val itemCompartir = menu?.findItem(R.id.share)
+        val shareActionProvider= MenuItemCompat.getActionProvider(itemCompartir) as ShareActionProvider
+        compartirIntent(shareActionProvider)
+
 
         vistaBusqueda.queryHint = getString(R.string.search_hint)
 
         vistaBusqueda.setOnQueryTextFocusChangeListener { v, hasFocus ->
-            Log.d("LISTENERFOUCS", hasFocus.toString())
+            Log.d("LISTENERFOCUS", hasFocus.toString())
         }
         vistaBusqueda.setOnQueryTextListener(object :SearchView.OnQueryTextListener,
             androidx.appcompat.widget.SearchView.OnQueryTextListener {
@@ -67,5 +73,14 @@ class MainActivity : AppCompatActivity() {
             else -> {return super.onOptionsItemSelected(item)}
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun compartirIntent(shareActionProvider: ShareActionProvider){
+        if(shareActionProvider != null){
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "Este es un mensaje compartido")
+            shareActionProvider.setShareIntent(intent)
+        }
     }
 }
---
+++ LVGVS/app/src/main/java/com/jodra/lvgvs/MainActivity.java
@@ -0,0 +1,110 @@
+package com.jodra.lvgvs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    MapView map = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }//handle permissions first, before map is created. not depicted here
+
+        //load/initialize the osmdroid configuration, this can be done
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
+
+        //inflate and create the map
+        setContentView(R.layout.activity_main);
+        try {
+
+
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        habilitarZoom(map);
+        puntoInicial(map,ubicacionActual());
+        }catch (Exception e){
+            e.getMessage();
+        }
+
+    }
+
+    public void onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    public void onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+
+    public void puntoInicial(MapView map,GeoPoint currentPoint) {
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(18.0);
+        GeoPoint startPoint = new GeoPoint(43.28758849480258, -2.979293540529369/*currentPoint*/);
+        mapController.setCenter(startPoint);
+    }
+
+    public void habilitarZoom(MapView map) {
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+    }
+
+    public GeoPoint ubicacionActual() {
+        GeoPoint puntoActual;
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        puntoActual = new GeoPoint(latitude,longitude);
+        Toast.makeText(this, latitude+"    :   "+longitude, Toast.LENGTH_LONG).show();
+        return puntoActual;
+    }
+
+
+}
